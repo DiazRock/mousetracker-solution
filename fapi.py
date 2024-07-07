9df076bf108c12695a3baea0fd3ce9531b7c25c3
DiazRock <corolariodiaz@gmail.com>
2024-07-07 07:26:39 -0400

    Added the endpoints in the fapi server

--- fapi.py
+++ fapi.py
@@ -1,7 +1,22 @@
-import asyncio
 from fastapi import FastAPI, WebSocket
-from fastapi.responses import HTMLResponse
+from starlette.responses import FileResponse
+from readMouse import readMouse
 
 
 app = FastAPI()
 
+
+@app.get("/")
+async def get():
+    return FileResponse('./frontend/index.html')
+
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    try:
+        while True:
+            position = next(readMouse())
+            await websocket.send_text(str(position))
+    except Exception as e:
+        print(f"Connection closed: {e}")
+        await websocket.close()
