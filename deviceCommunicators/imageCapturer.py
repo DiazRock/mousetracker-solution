906663b549203f3e2122b38df67fb49109e96628
DiazRock <corolariodiaz@gmail.com>
2024-07-08 18:15:22 +0300

    Changed the name of the folder to deviceCommunicators

---
+++ deviceCommunicators/imageCapturer.py
@@ -0,0 +1,18 @@
+import time
+import cv2
+
+
+class ImageCapturer:
+
+    @staticmethod
+    def capture_image():
+        print("Capturing image")
+        cam = cv2.VideoCapture(0)
+        ret, frame = cam.read()
+        if not ret:
+            print("Failed to grab frame")
+            return None
+        img_path = f'images/{int(time.time())}.png'
+        cv2.imwrite(img_path, frame)
+        cam.release()
+        return img_path
