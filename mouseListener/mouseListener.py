03b3d80221d96b4025db6f163dd4f742c16a8078
DiazRock <corolariodiaz@gmail.com>
2024-07-07 20:51:13 -0400

    Using pyserial now

--- mouseListener/mouseListener.py
+++ mouseListener/mouseListener.py
@@ -2,11 +2,14 @@ import asyncio
 import json
 from pynput import mouse
 import time
+import requests
+from serial import Serial
 
 class MouseListener:
 
     def __init__(self, clients: list) -> None:
         self.clients = clients
+        self.serial = Serial('/dev/input/mouse2', 9600, timeout = 1)
         pass
     
     @property
@@ -27,17 +30,26 @@ class MouseListener:
         self.clients.remove(client)
 
     def on_move(self, x, y):
-        time.sleep(0.01)
+        time.sleep(0.05)
         self.data = {'type': 'move', 'x': x, 'y': y}
-        asyncio.run(self.send_to_clients(self.data))
+        asyncio.run(self.send_to_clients())
 
     def on_click(self, x, y, button, pressed):
-        time.sleep(0.01)
+        time.sleep(0.05)
         self.data = {'type': 'click', 'x': x, 'y': y, 'button': str(button), 'pressed': pressed}
-        asyncio.run(self.send_to_clients(self.data))
+        asyncio.run(self.send_click_info(self.data))
 
-    async def send_to_clients(self, data):
-        self.data = json.dumps(data)
+    async def send_click_info(self, data):
+        dataToSend = { 'x': int(data['x']), 
+                       'y': int(data['y']), 
+                       'data_source_img': "foo"}
+        print(dataToSend)
+        with requests.post("http://localhost:8008/lclick", 
+                           json = dataToSend) as response:
+            print("The response ", response)
+
+    async def send_to_clients(self):
+        self.data = str(self.serial.readline())
         for client in self.clients:
             await client.send_text(self.data)
 
