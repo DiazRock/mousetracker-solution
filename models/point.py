8af00b2240c7b33eea649a8ad98f794757d83412
DiazRock <corolariodiaz@gmail.com>
2024-07-08 18:17:57 +0300

    Created model of the database

--- models/point.py
+++ models/point.py
@@ -1,6 +1,16 @@
-from pydantic import BaseModel
+from sqlalchemy import Column, String, Float
+from sqlalchemy.dialects.postgresql import UUID
+import dep_container
+import uuid
 
 
-class Point(BaseModel):
-    x: float
-    y: float
+class PointAndImage(dep_container.Base):
+    __tablename__ = 'PointAndImage'
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    image_source = Column(String, nullable=False)
+
+    # Vertical axis is the first component or the x
+    x_axis = Column(Float, nullable=False)
+
+    # Horizontal axis is the second component or the y
+    y_axis = Column(Float, nullable=False)
