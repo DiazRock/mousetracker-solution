92f2e38c54d3bf4ce80ea54507a689c1c05a5aa7
DiazRock <corolariodiaz@gmail.com>
2024-07-08 18:18:15 +0300

    Repository pattern implemented

---
+++ repositories/point_and_image_repository.py
@@ -0,0 +1,22 @@
+from typing import List, Optional
+from sqlalchemy.orm import Session
+from models.point import PointAndImage
+from schemas.point import Point
+
+
+class PointAndImageRepository:
+    def __init__(self, session: Session):
+        self.session = session
+
+    def create(self, data: Point) -> bool:
+        pointAndImage = PointAndImage(
+                        x_axis = data.x,
+                        y_axis = data.y,
+                        image_source = data.data_source_img)
+        self.session.add(pointAndImage)
+        self.session.commit()
+        self.session.refresh(pointAndImage)
+        return True
+
+    def get_all(self) -> List[Optional[Point]]:
+        return self.session.query(Point).all()
