0bd8abf284ce691c15f5f468694d2b2eadea67ee
DiazRock <corolariodiaz@gmail.com>
2024-07-11 00:52:46 +0300

    Changed the fapi server

--- fapi.py
+++ fapi.py
@@ -2,7 +2,9 @@ import threading
 import uvicorn
 import os
 import dep_container
-from fastapi import FastAPI, WebSocket, Depends
+import asyncio
+import json
+from fastapi import FastAPI, WebSocket
 from starlette.responses import FileResponse 
 from schemas.point import Point
 from dotenv import load_dotenv
@@ -10,10 +12,10 @@ from sqlalchemy.orm import Session
 from utils.init_db import create_tables
 
 
-
 load_dotenv()
 app = FastAPI()
-mouseListener = dep_container.get_listener([])
+publisher = dep_container.Publisher([])
+mouseEventTracker = dep_container.get_mouse_event_tracker(publisher)
 imageCapturer = dep_container.get_image_capturer()
 logger = dep_container.get_logger()
 
@@ -23,11 +25,17 @@ def get():
     logger.info("Serving main view page")
     return FileResponse('./frontend/index.html')
 
+@app.on_event("startup")
+def on_startup() -> None:
+    """
+    Initializes the database tables when the application starts up.
+    """
+    create_tables()
 
-@app.post("/lclick")
-async def liclick(
+
+async def lclick(
                 point: Point,
-                session: Session = Depends(dep_container.get_db)):
+                session: Session):
     logger.info(f"Received left click at {point}")
     image_path = imageCapturer.capture_image()
     point.data_source_img = image_path
@@ -37,31 +45,29 @@ async def liclick(
     return image_path
 
 
-@app.on_event("startup")
-def on_startup() -> None:
-    """
-    Initializes the database tables when the application starts up.
-    """
-    create_tables()
-
-
 @app.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
     await websocket.accept()
-    mouseListener.add_client(websocket)
+    publisher.add_client(websocket)
     logger.info("Connection accepted")
     try:
         while True:
-            data = await websocket.receive_text()
-            logger.info(f"Data received from websocket, {data}")
-            await websocket.send_text(f"Message text was: {data}")
+            raw_data = await websocket.receive_text()
+            data = json.loads(raw_data)
+            logger.info(f"Data received from websocket: {data}")
+            if data['event'] == 'lclick':
+                logger.info("Received left click event")
+                db_session = next(dep_container.get_db())
+                await lclick(Point(x=data['x'], y=data['y']), session=db_session)
+            asyncio.sleep(os.getenv('ELAPSED_TIME'))
+
     except Exception as e:
         logger.error(f"Connection error: {e}")
     finally:
-        mouseListener.remove_client(websocket)
+        publisher.remove_client(websocket)
         logger.info(f"Connection closed in {websocket.url}")
 
+
 if __name__ == "__main__":
-    threading.Thread(target=mouseListener.start_mouse_listener, daemon=True).start()
-    
+    threading.Thread(target=mouseEventTracker.start_mouse_listener, daemon=True).start()
     uvicorn.run(app, host=os.getenv("SERVER_HOST"), port=int(os.getenv("SERVER_PORT")))
